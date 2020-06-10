961e1d1782331b61414fd84208696e973ae6185a
Erik Dymbe <erik.dymbe@gmail.com>
2020-06-10 03:17:12 +0200

    Add option to plot circles

--- cias.py
+++ cias.py
@@ -6,14 +6,28 @@ from scipy.spatial.distance import cdist
 def evaluate(x):
     points = as_points(x)
     dists = cdist(points, points)
-    return dists[np.triu_indices(dists.shape[0], 1, dists.shape[0])].min()
+    return np.min(dists[np.triu_indices(dists.shape[0], 1, dists.shape[0])])
+
+
+def negative_evaluate(x):
+    return -evaluate(x)
 
 
 def as_points(x):
     return np.asarray([[x[i], x[i+1]] for i in range(0, len(x), 2)])
 
 
-def plot(x):
+def plot(x, circles=False):
     points = as_points(x)
+    _, axes = plt.subplots()
+    plt.xlim([0, 1])
+    plt.ylim([0, 1])
     plt.scatter(points[:, 0], points[:, 1])
+    score = evaluate(x)
+    plt.title("Fitness: {:10.4f}".format(score))
+    if circles:
+        axes.set_aspect(1)
+        for x, y in points:
+            draw_circle = plt.Circle((x, y), score / 2, fill=False)
+            axes.add_artist(draw_circle)
     plt.show()
