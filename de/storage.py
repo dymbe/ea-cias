ccab73462ba1e5fc0d1814a1651418de5cbc88d9
Erik Dymbe <erik.dymbe@gmail.com>
2020-06-26 22:54:29 +0200

    Add result-fetching from file

--- de/storage.py
+++ de/storage.py
@@ -7,7 +7,7 @@ import numpy as np
 # Finds a stored result of a run
 def find_stored(pop, n, f, cr, path="out"):
     for file in [x for x in listdir(path) if isfile(join(path, x))]:
-        data = np.loadtxt(f"{path}/{file}", delimiter=",").reshape(-1, 7)
+        data = np.loadtxt(f"{path}/{file}", delimiter=",").reshape(-1, 8)
         for row in data:
             if row[2] == cr and row[3] == f and row[4] == n and row[5] == pop:
                 print("Saved result found")
